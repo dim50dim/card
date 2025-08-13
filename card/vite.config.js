c65470089ad778500853b9467975be4a24c223dd
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-13 21:52:15 +0200

    added path.__dirname in vite.config.js

--- card/vite.config.js
+++ card/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(),tailwindcss()],
   resolve:{
     alias:{
-      "@":path.resolve(__dirname,"./src")
+      "@":path.resolve(path.__dirname,"./src")
     }
   }
 })
