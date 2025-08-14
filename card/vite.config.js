4e6990750ef0db1de532943cb400ef199ba77094
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-14 07:56:48 +0200

    so, have got misunderstanding with tailwindcss.

--- card/vite.config.js
+++ card/vite.config.js
@@ -1,13 +1,32 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from "tailwindcss/vite"
+// import path from "path";
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),tailwindcss()],
+//   resolve:{
+//     alias:{
+//       "@":path.resolve(path.__dirname,"./src")
+//     }
+//   }
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "tailwindcss/vite"
-import path from "path";
-// https://vite.dev/config/
+// import tailwindcss from 'tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const tailwindcss = (await import('tailwindcss/vite')).default
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve:{
-    alias:{
-      "@":path.resolve(path.__dirname,"./src")
-    }
-  }
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
