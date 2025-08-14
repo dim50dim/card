25f34766c3d23e9b66e15f2f1f3be495111396cd
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-14 21:05:16 +0200

    added localStorage to condition , for remembering the exact theme.

--- card/src/components/ThemeToggle.jsx
+++ card/src/components/ThemeToggle.jsx
@@ -1,16 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {Moon,Sun} from "lucide-react";
 
 
 const ThemeToggle = () => {
+
     const [isDarkMode,setIsDarkMode] = useState(false)
+  
+    useEffect(() => {
+       const storedTheme = localStorage.getItem("theme")
+       if(storedTheme === "dark"){
+        setIsDarkMode(true)
+       }
+    },[])
+
     const toggleTheme =() => {
         if(isDarkMode) {
           document.documentElement.classList.add("dark")
+          localStorage.setItem("theme","light")
+
             setIsDarkMode(false)
 
         }else{
           document.documentElement.classList.remove("dark")
+          localStorage.setItem("theme","dark")
             setIsDarkMode(true)
         }
     }
