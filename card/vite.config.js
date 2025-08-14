ac4534da412acac83cd07bf9f67faf12af793054
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-14 20:56:13 +0200

    so, the result of the dev deploy is positive , and this is , because i had deleted the line with tailwindcss from vite.config.js

--- card/vite.config.js
+++ card/vite.config.js
@@ -1,46 +1,14 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from "tailwindcss/vite"
-// import path from "path";
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(),tailwindcss()],
-//   resolve:{
-//     alias:{
-//       "@":path.resolve(path.__dirname,"./src")
-//     }
-//   }
-// })
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from 'tailwindcss/vite'
-// import path from 'path'
-// import { fileURLToPath } from 'url'
-
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
-
-// // const tailwindcss = (await import('tailwindcss/vite')).default
-
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, './src'),
-//     },
-//   },
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss/vite'
+// import tailwindcss from 'tailwindcss/vite'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
